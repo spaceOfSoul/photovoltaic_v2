9bef6f2fb4fa962c2690e92bb22a34ab6f07bb7f
spaceOfSoul <ychh1123@gmail.com>
2024-03-07 09:10:48 +0900

    rcnn test

--- tests.sh
+++ tests.sh
@@ -3,5 +3,5 @@
 #SBATCH --output=test_multiple_output.txt
 
 
-python main.py --mode test --model correction_LSTMs --load_path train_models/2-stageLL_2000_drop0.5_2/best_model --save_dir train_models/2-stageLL_2000_drop0.5_2 --log_filename test_record.txt
-python main.py --mode test --model correction_LSTMs --load_path train_models/2-stageLL_2000_drop0.5_3/best_model --save_dir train_models/2-stageLL_2000_drop0.5_3 --log_filename test_record.txt
+python main.py --mode test --model RCNN --load_path train_models/RCNN_2000_drop0.5_2/best_model --save_dir train_models/RCNN_2000_drop0.5_2 --log_filename test_record.txt
+python main.py --mode test --model RCNN --load_path train_models/RCNN_2000_drop0.5_3/best_model --save_dir train_models/RCNN_2000_drop0.5_3 --log_filename test_record.txt
